a19809797dac68220bb84a2d0b20d3803ba7bf5f
Jasjeev <jasjeev99@gmail.com>
2024-08-15 20:00:32 +0530

    Server Intialized

---
+++ server/src/test/java/com/hustle_hub/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hustle_hub.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
